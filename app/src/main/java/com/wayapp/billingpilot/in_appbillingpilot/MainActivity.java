21cf296469a03c7844754aec31284b7f723f657e
Luis <lsantiago@wayapp.com>
2017-10-19 02:08:34 -0500

    Initial commit

--- app/src/main/java/com/wayapp/billingpilot/in_appbillingpilot/MainActivity.java
+++ app/src/main/java/com/wayapp/billingpilot/in_appbillingpilot/MainActivity.java
@@ -3,8 +3,10 @@ package com.wayapp.billingpilot.in_appbillingpilot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import com.wayapp.billingpilot.in_appbillingpilot.util.IabHelper;
 import com.wayapp.billingpilot.in_appbillingpilot.util.IabResult;
+import com.wayapp.billingpilot.in_appbillingpilot.util.Inventory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +19,41 @@ public class MainActivity extends AppCompatActivity {
     static final String SKU_100 = "test_buy_100";
     static final String SKU_500 = "test_buy_500";
 
+    TextView cost100Credits;
+    TextView cost500Credits;
+    TextView actualCredits;
+
     IabHelper mHelper;
+
+    IabHelper.QueryInventoryFinishedListener
+            mQueryFinishedListener = new IabHelper.QueryInventoryFinishedListener() {
+        @Override
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory)
+        {
+            if (result.isFailure()) {
+                // handle error
+                return;
+            }
+
+            String price100Credits =
+                    inventory.getSkuDetails(SKU_100).getPrice() + " EUR";
+            String price500Credits =
+                    inventory.getSkuDetails(SKU_500).getPrice() + " EUR";
+
+            cost100Credits.setText(price100Credits);
+            cost500Credits.setText(price500Credits);
+
+        }
+    };
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cost100Credits = (TextView) findViewById(R.id.cost100Credits);
+        cost500Credits = (TextView) findViewById(R.id.cost500Credits);
 
         String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArxCSiBvvQK3iK0p7Px0G8nW2lMPM3pBweAJpugtbvbsAlfWozIJis8mYuXv4DTPSVHaRktVUzg15mzlZPKRs+87oWcTN1J2NkaLeU5Wkpl8/6IdDIkBlDsg3h7DveJdt6tMinZOSHsN566IuUSzo5O1MyCkTxUA3O7jAfDYQZwqFeIHy3w9RB91N8T8Bl7dImhlAD68hnxSpL96fLPulDmFS/iEV7K1PGONos2eDEY+rOSaqY0zbIx/qvi4e1yC19RpzzdnFKLRmoVEqsESX3p2haBnYHi84q36ezKGW7qnz9G0m7ZzkobrgCOYJjYZhhuycLJYAj7rZeoa8Jt/lowIDAQAB";
 
@@ -42,8 +73,13 @@ public class MainActivity extends AppCompatActivity {
         List additionalSkuList = new ArrayList();
         additionalSkuList.add(SKU_100);
         additionalSkuList.add(SKU_500);
-        mHelper.queryInventoryAsync(true, additionalSkuList,
-                mQueryFinishedListener);
+
+        try {
+            mHelper.queryInventoryAsync(true, additionalSkuList, null, mQueryFinishedListener);
+        } catch (IabHelper.IabAsyncInProgressException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
